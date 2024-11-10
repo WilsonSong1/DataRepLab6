7195aeebe05084eb133cdda36f9751ba9f73ac7d
WilsonSong1 <G00418330@atu.ie>
2024-11-10 21:12:21 +0000

    Exercise 5, installed body-parser, made create.js file

--- my-app/BackEnd/server.js
+++ my-app/BackEnd/server.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 const port = 4000;
 
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
 app.get('/api/movies', (req, res) => {
     const Movies = [
         {
@@ -35,14 +50,4 @@ app.get('/api/movies', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
-
-const cors = require('cors');
-app.use(cors());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
 });
