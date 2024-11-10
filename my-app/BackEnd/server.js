0045d84c8b4f18aec7fbb0a10920272d297824db
WilsonSong1 <G00418330@atu.ie>
2024-11-10 21:08:30 +0000

    Exercise 4, installed CORS and enabled cors for my server

--- my-app/BackEnd/server.js
+++ my-app/BackEnd/server.js
@@ -36,3 +36,13 @@ app.get('/api/movies', (req, res) => {
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
